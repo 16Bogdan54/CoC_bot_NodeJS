d3aaf129d5fe6e5e8bba831ab014b1fa37474561
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-03-11 18:21:58 +0200

    input test

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,23 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start((ctx) => ctx.reply('Welcome'))
 bot.help((ctx) => ctx.reply('Help'))
 
+bot.command('getclan', async  (ctx) => await ctx.reply('Enter clan tag in format - clan: #123abc'))
+bot.command('getplayer', async (ctx) => await ctx.reply('Enter player tag in format - player: #123abc'))
+
+bot.on('text', async (ctx) => {
+    const userInput: string = ctx.message.text
+
+    console.log(userInput)
+
+    if(userInput.includes('clan:')) {
+        await ctx.reply('Clan tag ' + userInput.slice(userInput.indexOf('#')+1, userInput.length))
+    }
+
+    if(userInput.includes('player:')) {
+        await ctx.reply('Player tag ' + userInput.slice(userInput.indexOf('#')+1, userInput.length))
+    }
+
+})
 
 bot.on('sticker', (ctx) => ctx.reply('👍'))
 
